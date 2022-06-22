67ab416950ba730be4530a58385b90afd5590072
JetJadeja <jetjadeja@gmail.com>
2022-06-22 16:05:52 -0700

    Add SimpleStore test contract

---
+++ test/SimpleStore.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+contract SimpleStoreTest is Test {
+    /// @dev Address of the SimpleStore contract.  
+    SimpleStore public simpleStore;
+
+    /// @dev Setup the testing environment.
+    function setUp() public {
+        simpleStore = SimpleStore(new HuffDeployer().deploy("SimpleStore"));
+    }
+
+    /// @dev Ensure that you can set and get the value.
+    function testSetAndGetValue(uint256 value) public {
+        simpleStore.setValue(value);
+        console.log(value);
+        console.log(simpleStore.getValue());
+        assertEq(value, simpleStore.getValue());
+    }
+}
+
+interface SimpleStore {
+    function setValue(uint256) external;
+    function getValue() external returns (uint256);
+}
